b3609475ae6660c924c4fc4cf25917e26775b5dc
himanshujays29 <himanshujayswal2006@gmail.com>
2025-12-11 16:36:25 +0530

    comment fix

--- middleware.js
+++ middleware.js
@@ -1,5 +1,4 @@
 export const isLoggedIn = (req, res, next) => {
-  // Check our custom session property set in server.js
   if (!req.session.userId) {
     if (req.originalUrl.startsWith("/api")) {
       return res.status(401).json({ error: "Unauthorized" });
